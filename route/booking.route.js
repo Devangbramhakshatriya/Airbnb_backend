b487ea2f02c706e33daddb3e8639374bd0fafeb2
Devang Bramhakshatriya <devangbramhakshtriya@gmail.com>
2023-08-12 11:04:42 +0530

    second commit

--- route/booking.route.js
+++ route/booking.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { auth } = require('../middleware/auth.midddleware');
-const { bookingAdd, bookingGetAll, deletebooking, bookingGet } = require('../controller/booking.controller');
+const { bookingAdd, bookingGetAll, deletebooking, bookingGet, getSingleBooking } = require('../controller/booking.controller');
 const booking = express.Router();
 
 booking.post("/booking",auth,bookingAdd)
@@ -8,4 +8,5 @@ booking.post("/adminbooking", bookingAdd)
 booking.get("/",bookingGet)
 booking.get("/getbooking",auth,bookingGetAll)
 booking.delete("/delete/:id",deletebooking)
+booking.get("/singlebooking/:id",getSingleBooking)
 module.exports={booking}
